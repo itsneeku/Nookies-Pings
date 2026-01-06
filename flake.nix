24efb675efcc29f8c491b470dfb3b5b3b7ea9833
itsneeku <nicolae@rusu.dev>
2026-01-05 21:13:19 -0500

    ops: flake apps

--- flake.nix
+++ flake.nix
@@ -20,7 +20,32 @@
           ];
         };
 
+        apps = {
+          db = utils.lib.mkApp {
+            drv = pkgs.writeShellScriptBin "start-db" ''
+              exec ${pkgs.valkey}/bin/valkey-server
+            '';
+          };
 
+          ui = utils.lib.mkApp {
+            drv = pkgs.writeShellScriptBin "start-ui" ''
+              exec ${pkgs.deno}/bin/deno run --allow-all --watch src/ui.ts
+            '';
+          };
+
+          main = utils.lib.mkApp {
+            drv = pkgs.writeShellScriptBin "start-main" ''
+              exec ${pkgs.deno}/bin/deno run --allow-all --watch src/main.ts
+            '';
+          };
+
+          worker = utils.lib.mkApp {
+            drv = pkgs.writeShellScriptBin "start-worker" ''
+              exec ${pkgs.deno}/bin/deno run --allow-all --watch src/worker.ts
+            '';
+          };
+
+        };
       }
     );
 }
