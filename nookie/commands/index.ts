d2d59e525bd318566698e61f3444245bd8700cb3
itsneeku <nicolae@rusu.dev>
2026-01-14 01:23:11 -0500

    feat: dont require explicit DISCORD_APPLICATION_ID

--- nookie/commands/index.ts
+++ nookie/commands/index.ts
@@ -1,15 +1,20 @@
-import { REST, Routes } from "discord.js";
+import { REST, RESTGetCurrentApplicationResult, Routes } from "discord.js";
 import monitor from "nookie/commands/monitor";
 
 export const commands = new Map([monitor].map((cmd) => [cmd.data.name, cmd]));
 
-export const deployCommands = async () =>
-  await new REST()
-    .setToken(process.env.DISCORD_TOKEN)
-    .put(
-      Routes.applicationGuildCommands(
-        process.env.DISCORD_APPLICATION_ID,
-        process.env.DISCORD_GUILD_ID
-      ),
-      { body: Array.from(commands.values()).map((cmd) => cmd.data.toJSON()) }
-    );
+export const deployCommands = async () => {
+  const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+  const applicationId = (
+    (await rest.get(
+      Routes.currentApplication()
+    )) as RESTGetCurrentApplicationResult
+  ).id;
+  await rest.put(
+    Routes.applicationGuildCommands(
+      applicationId,
+      process.env.DISCORD_GUILD_ID
+    ),
+    { body: Array.from(commands.values()).map((cmd) => cmd.data.toJSON()) }
+  );
+};
