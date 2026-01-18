c428cadb41b8f9e0d5e0b36329d597a366699ce4
itsneeku <nicolae@rusu.dev>
2026-01-18 01:28:29 -0500

    feat(ebgames.ca): hit uncached endpoint efficiently under rate limit

--- scrapers/ebgames.ca.py
+++ scrapers/ebgames.ca.py
@@ -2,36 +2,72 @@ from scrapers.utils.base import ScrapedProduct
 import zendriver as zd
 import asyncio
 
-patterns = ["ebgames.ca"]
+CONFIG = zd.Config(
+  headless=False,
+  browser_args=[
+    "--blink-settings=imagesEnabled=false",
+  ],
+)
+PATTERNS = ["ebgames.ca"]
 
 
-# product id != sku
-# product id is the page identifier (https://www.ebgames.ca/Trading%20Cards/Games/941649/)
-# sku is the product code (803752) (we dont care about it)
-async def product(url):
-  browser = await zd.start(headless=True)
-  page = await browser.get(url)
-  await page
-  await page.select("body")
+async def product_page(url):
+  config = zd.Config(
+    headless=False,
+    browser_args=[
+      "--blink-settings=imagesEnabled=false",
+    ],
+  )
+  browser = await zd.start(config)
+  page = browser.main_tab
+  await page.send(zd.cdp.emulation.set_data_saver_override(data_saver_enabled=True))
+  await page.get(url)
+  await page.wait_for("body")
 
-  return ScrapedProduct(
+  res = ScrapedProduct(
     sku=(await page.select("body")).get("class_").split("-")[-1],
     url=(await page.query_selector('link[rel="canonical"]')).get("href"),
     title=(await page.select(".prodTitle")).text.strip(),
     inStock="Deliver to Home" in (await page.select(".productAvailabilityNew")).text,
-    price=(await page.select("span.pricetext")).text.replace("$", "").strip(),
+    price=(await page.select("span.prodPriceCont")).text.replace("$", "").strip(),
     image=(await page.select("#packshotImage")).get("src"),
   )
+  await browser.stop()
+  return res
+
+
+async def fetch(page: zd.Tab, sku: str):
+  """
+  Rate limited @ 500 queries/5 mins
+  """
+  response = await page.evaluate(
+    f"""
+        fetch('https://www.ebgames.ca/api2/product/getproductsbysku?skus[]={sku}', {{
+            headers: {{"X-Requested-With": "XMLHttpRequest"}}
+        }}).then(response => response.json())
+        """,
+    await_promise=True,
+  )
+  return response
+
+
+async def get_product_sku(page: zd.Tab, url: str):
+  await page.get(url)
+  await page.wait_for_ready_state("complete")
+  return (await page.select(".variantSku")).get("value")
 
 
 async def main():
-  print(await product("https://www.ebgames.ca/Trading%20Cards/Games/941968"))
-  print(await product("https://www.ebgames.ca/Trading Cards/Games/941968"))
-  print(
-    await product(
-      "https://www.ebgames.ca/Trading%20Cards/Games/941968/pokemon-trading-card-game-mega-venusaur-ex-premium-collection-french"
-    )
+  browser = await zd.start(CONFIG)
+  page = browser.main_tab
+  await page.send(zd.cdp.emulation.set_data_saver_override(data_saver_enabled=True))
+  sku = await get_product_sku(
+    page,
+    "https://www.ebgames.ca/Trading%20Cards/Games/961429/magic-the-gathering-teenage-mutant-ninja-turtles-booster-box",
   )
+  result = await fetch(page, sku)
+  print(result)
+  await browser.stop()
 
 
 if __name__ == "__main__":
