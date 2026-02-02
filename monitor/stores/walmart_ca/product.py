464b14d9cb2fa3f8eb5bdfc7187f7c2060d20ec5
itsneeku <nicolae@rusu.dev>
2026-02-02 18:00:19 -0500

    fix(walmart_ca/search): don't depend on curl_cffi

--- monitor/stores/walmart_ca/product.py
+++ monitor/stores/walmart_ca/product.py
@@ -1,37 +1,34 @@
-from monitor.utils.base import get_random_ipv6
 from monitor.utils.ssr import (
-  extract_next_ssr_data_html,
   extract_next_ssr_data_zendriver,
 )
 import zendriver as zd
-import curl_cffi
+import asyncio
 
 
 async def main(input):
   url = f"https://www.walmart.ca/en/ip/{input['sku']}"
-  data = None
-  response = curl_cffi.get(
-    url, interface=get_random_ipv6(), impersonate="safari_ios", timeout=20
+
+  browser = await zd.start(
+    headless=False,
   )
+  page = await browser.get(url)
 
-  if response.status_code == 200:
-    data = extract_next_ssr_data_html(response.text)
-  else:
-    browser = await zd.start(headless=True)
-    page = await browser.get(url)
-    await page
-    data = await extract_next_ssr_data_zendriver(page)
-
+  data = await extract_next_ssr_data_zendriver(page)
   item = (
     data.get("props").get("pageProps").get("initialData").get("data").get("product")
   )
-  sold_by_wm = item.get("sellerId") == "0"
 
-  return {
+  sold_by_wm = item.get("sellerId") == "0"
+  in_stock = item.get("availabilityStatusV2", {}).get("value") == "IN_STOCK"
+
+  data = {
     "title": item.get("name"),
-    "inStock": 1
-    if sold_by_wm and item.get("availabilityStatusV2").get("value") == "IN_STOCK"
-    else 0,
-    "price": item.get("priceInfo").get("currentPrice").get("price"),
-    "image": item.get("imageInfo").get("thumbnailUrl"),
+    "inStock": sold_by_wm and in_stock,
+    "price": item.get("priceInfo", {}).get("currentPrice", {}).get("price"),
+    "image": item.get("imageInfo", {}).get("thumbnailUrl"),
   }
+  return data
+
+
+if __name__ == "__main__":
+  asyncio.run(main({"sku": "70PH2R4R8A83"}))
